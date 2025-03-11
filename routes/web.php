711053ced978b8b673be17ce19a709809b6a085f
muhammadrosyidd <rosyidmuhammad612@gmail.com>
2025-03-11 14:53:42 +0700

    praktikum-2.6

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,9 @@ Route::get('/penjualan',[PenjualanController::class, 'index']);
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/tambah', [UserController::class,'tambah']);
+Route::get('/user/ubah/{id}', [UserController::class,'ubah']);
+Route::get('/user/hapus/{id}', [UserController::class,'hapus']);
+Route::post('/user/tambah_simpan', [UserController::class,'tambah_simpan']);
+Route::put('/user/ubah_simpan/{id}', [UserController::class,'ubah_simpan']);
+
