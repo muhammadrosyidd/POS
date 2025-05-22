fb7258a93e46367aca61f9269337b452e849e3a5
muhammadrosyidd <rosyidmuhammad612@gmail.com>
2025-05-23 01:26:06 +0700

    JS10-Prak4

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LevelController;
+use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LogoutController;
+use App\Http\Controllers\Api\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +17,14 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
-Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
-Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+Route::post('/register', [RegisterController:: class, '__invoke'])->name('register');
+Route::post('/login', [LoginController:: class, '__invoke'])->name('login');
+Route::post('/logout', [LogoutController:: class, '__invoke'])->name('logout');
+Route::get('levels', [LevelController:: class, 'index']);
+Route::post('levels', [LevelController:: class, 'store' ]);
+Route::get('levels/{level}', [LevelController:: class, 'show' ]);
+Route::put('levels/{level}', [LevelController::class, 'update' ]);
+Route::delete('levels/{level}', [LevelController:: class, 'destroy']);
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
