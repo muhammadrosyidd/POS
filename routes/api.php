c385a61c56f6d6dd43e6b6cd6028f3507d4d08ab
muhammadrosyidd <rosyidmuhammad612@gmail.com>
2025-05-23 01:28:43 +0700

    JS10-Tugas

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\Api\LevelController;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\LogoutController;
@@ -20,11 +23,34 @@ use App\Http\Controllers\Api\RegisterController;
 Route::post('/register', [RegisterController:: class, '__invoke'])->name('register');
 Route::post('/login', [LoginController:: class, '__invoke'])->name('login');
 Route::post('/logout', [LogoutController:: class, '__invoke'])->name('logout');
+
 Route::get('levels', [LevelController:: class, 'index']);
 Route::post('levels', [LevelController:: class, 'store' ]);
 Route::get('levels/{level}', [LevelController:: class, 'show' ]);
 Route::put('levels/{level}', [LevelController::class, 'update' ]);
 Route::delete('levels/{level}', [LevelController:: class, 'destroy']);
+
+//users
+Route::get('users', [UserController::class, 'index']);
+Route::post('users', [UserController::class, 'store']);
+Route::get('users/{id}', [UserController::class, 'show']);
+Route::put('users/{id}', [UserController::class, 'update']);
+Route::delete('users/{id}', [UserController::class, 'destroy']);
+
+//kategoris
+Route::get('kategoris', [KategoriController::class, 'index']);
+Route::post('kategoris', [KategoriController::class, 'store']);
+Route::get('kategoris/{id}', [KategoriController::class, 'show']);
+Route::put('kategoris/{id}', [KategoriController::class, 'update']);
+Route::delete('kategoris/{id}', [KategoriController::class, 'destroy']);
+
+//barangs
+Route::get('barangs', [BarangController::class, 'index']);
+Route::post('barangs', [BarangController::class, 'store']);
+Route::get('barangs/{id}', [BarangController::class, 'show']);
+Route::put('barangs/{id}', [BarangController::class, 'update']);
+Route::delete('barangs/{id}', [BarangController::class, 'destroy']);
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
